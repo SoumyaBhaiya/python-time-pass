a4a82d18010b099cfe0cbe8039e9dee361c47e72
Soumya Bhaiya <soumybhaiya@gmail.com>
2022-10-18 00:01:01 +0530

    Added formatted string

--- pytp.py
+++ pytp.py
@@ -2,3 +2,6 @@ print("hello world")
 
 name = input("Name: ")
 print("Your current name is: ", name)
+print(f"hello, {name}")
+#Here f is formatted string and the curly braces means we r saying to plugin value
+#of name
