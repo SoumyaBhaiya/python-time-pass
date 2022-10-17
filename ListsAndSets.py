31f7bea1a6dd2fa6be447138add049f42e7f8ea3
Soumya Bhaiya <soumybhaiya@gmail.com>
2022-10-18 00:40:25 +0530

    renamed the file and added some code

---
+++ ListsAndSets.py
@@ -0,0 +1,12 @@
+#Made a list
+names = ["Harry", "Ron", "Hermoine", "Ginny"]
+
+print(names)
+print(names[0])
+print("-_-_-________________________________")
+names.append("Draco")
+#append adds a value in lists
+print(names)
+print("---------------------------------")
+names.sort()
+print(names)
