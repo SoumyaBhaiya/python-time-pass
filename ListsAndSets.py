f1f21491b67c0383ac68148b438a3151ba750be8
Soumya Bhaiya <soumybhaiya@gmail.com>
2022-10-18 00:50:24 +0530

    Done in lists and sets

--- ListsAndSets.py
+++ ListsAndSets.py
@@ -10,3 +10,25 @@ print(names)
 print("---------------------------------")
 names.sort()
 print(names)
+
+
+#Create an empty set
+
+s = set()
+
+#Add some elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+print(s)
+#Notice that 3 did't appeared twice. 
+# every element will not repeat itself
+print("----------------")
+s.remove(2)
+print(s)
+
+#len will give you length of a sequence or number of characters or so
+
+print(f"The set has {len(s)} elements")
