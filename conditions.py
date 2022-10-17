81043969dbb59eb17c94f5c0cd59c2ab0d9d503c
Soumya Bhaiya <soumybhaiya@gmail.com>
2022-10-18 00:31:51 +0530

    0.01% Progress done

--- conditions.py
+++ conditions.py
@@ -4,3 +4,37 @@ if num >= 18:
     print("You are eligible to vote")
 else:
     print("You are not eligible to vote")
+    
+    
+#Now time for sequences
+
+name = "Megan Fox"
+print(name[0])
+
+for i in name:
+    print(name)
+
+    
+#Now what are data structure
+#We will see list first
+
+names = ["Harry", "Ron", "Hermione"]
+print("----------------------------------------")
+
+#Now this is list
+#Lists are mutable i.e. they can be changed
+print(names)
+print(names[2])
+
+#Now we will see tuples
+print("------------------------------------")
+coordinates = (10.0, 20.0)
+
+#This is a tuple and tuples are immutable i.e they can't be changed
+print(coordinates)
+
+
+
+
+
+
