0c64479fe008fec3b602aa5add84c5ca496c33cd
Soumya Bhaiya <soumybhaiya@gmail.com>
2022-10-19 16:05:25 +0530

    Added code for creating dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+#Understanding Dictionary
+
+houses = {"Harry": "Gryffindor", "Draco": "Slytherine"}
+
+print(houses["Harry"])
+
+#Now if we wanna add something to a dictionary we will do like this
+
+houses["Hermoine"] = "Gryffindor"
+print(houses["Hermoine"])
